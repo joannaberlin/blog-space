03875318cd1d67fabb80b3f0c145c77b8fea914a
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-05 10:35:28 +0100

    Render posts from fetched data

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const postsWrapper = document.getElementById('blog-list');
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 	.then((response) => response.json())
 	.then((data) => {
 		const postsArr = data.slice(0, 5);
-		console.log(postsArr);
+		let html = '';
+		for (let post of postsArr) {
+			html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            `;
+		}
+		postsWrapper.innerHTML = html;
 	});
