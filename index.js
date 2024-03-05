06974d34fe5705fa94ab903b73c513146bc6ff78
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-05 16:06:39 +0100

    Add event listener on the form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const postsWrapper = document.getElementById('blog-list');
+const newPost = document.getElementById('new-post');
 
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 	.then((response) => response.json())
@@ -8,8 +9,19 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 		for (let post of postsArr) {
 			html += `
                 <h3>${post.title}</h3>
-                <p>${post.body}</p>
+                <div id='body_wrapper'><p>${post.body}</p></div>
             `;
 		}
 		postsWrapper.innerHTML = html;
 	});
+
+newPost.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const postTitle = document.getElementById('post-title').value;
+	const postBody = document.getElementById('post-body').value;
+	const data = {
+		title: postTitle,
+		body: postBody,
+	};
+	console.log(data);
+});
